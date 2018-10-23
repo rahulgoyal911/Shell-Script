0ac3338906ea3a4f374a95b1182c8953a73c3d9b
rahulgoyal911 <rahulgoyal0.rg@gmail.com>
2018-10-23 13:18:01 +0530

    itl

---
+++ exp31.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Enter the number"
+read n
+while [ $n != 0 ]
+do
+n=$(( $n - 1 ))
+echo $n
+done
---
+++ test.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#while loop
+#read num
+#while [ $num -gt 0 ]
+#do
+#echo "$num"
+#num=$(( $num -1 ))
+#done
+
+#for loop
+#for i in 5 4 3 2 1
+#do
+#echo -e "$i"
+#done
+
+#factorial
+#read n
+#fact=1
+#for((i=$n;i>=1;i--))
+#do
+#fact=`expr $fact \* $i`
+#done
+#echo $fact
+
+
+#compare files
+#if diff a b >/dev/null ; then
+#echo "same"
+#else
+#echo "diff"
+#fi
+
+
+#sum of all digits
+#read num
+#sum=0
+#while [ $num -gt 0 ]
+#do
+#rem=$(($num % 10))
+#sum=$(($sum + $rem ))
+#num=$(($num / 10))
+#done
+#echo "$sum"
+
+
+#for loop
+#a=5
+#for((i=$a;i>=1;i--))
+#do
+#echo "$i"
+#done
+
+
+
+
+
+
+
+
